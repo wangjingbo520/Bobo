3d589ec2766400e752adc34c5333de726f8f4b36
王静波 <wangjingbo3@qq.com>
2019-09-23 11:39:33 +0800

    Update VolleyProcessor.java

--- app/src/main/java/com/zynet/bobo/mvc/http/VolleyProcessor.java
+++ app/src/main/java/com/zynet/bobo/mvc/http/VolleyProcessor.java
@@ -2,13 +2,14 @@ package com.zynet.bobo.mvc.http;
 
 import android.content.Context;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.zynet.bobo.MyApplication;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -20,24 +21,40 @@ public class VolleyProcessor implements IHttpProcessor {
 
     private static RequestQueue mQueue = null;
 
+    private static final int sTimeOut = 30000;
+    //是否使用Volley默认连接超时
+    private boolean timeOutDefaultFlg = false;
+    private int myTimeOut = timeOutDefaultFlg ? DefaultRetryPolicy.DEFAULT_TIMEOUT_MS : sTimeOut;
+
     public VolleyProcessor(Context context) {
-        mQueue = Volley.newRequestQueue(context);
+        mQueue = Volley.newRequestQueue(context.getApplicationContext());
     }
 
     @Override
     public void post(String url, Map<String, Object> params, ICallBack callBack) {
+        HashMap<String, String> hashMap = new HashMap<>();
+        for (Map.Entry<String, Object> enty : params.entrySet()) {
+            hashMap.put(enty.getKey(), enty.getValue().toString());
+        }
         StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
-                callBack::onSucess, error -> callBack.onFailure());
+                callBack::onSucess, error -> callBack.onFailure()) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                return hashMap;
+            }
+        };
+        stringRequest.setRetryPolicy(new DefaultRetryPolicy(myTimeOut,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
         mQueue.add(stringRequest);
     }
 
     @Override
-    public void get(String url) {
-
+    public void get(String url, ICallBack callBack) {
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url
+                , callBack::onSucess, error -> callBack.onFailure());
+        stringRequest.setRetryPolicy(new DefaultRetryPolicy(myTimeOut,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        mQueue.add(stringRequest);
     }
 
-//    public void removeRequest(){
-//        if (mQueue!=null||)
-//        MyApplication.getRequestQueue().cancelAll(VOLLEY_TAG);
-//    }
 }
