45ee9136b222254a0e73b8a3c2bbcb2f62fec880
王静波 <wangjingbo3@qq.com>
2019-10-14 11:29:39 +0800

    Create IBaseView.java

---
+++ app/src/main/java/com/zynet/bobo/base/IBaseView.java
@@ -0,0 +1,18 @@
+package com.zynet.bobo.mvp;
+
+public interface IBaseView {
+    //显示进度中
+    void showLoading();
+
+    //显示请求成功
+    void showSuccess();
+
+    //失败重试
+    void showFaild();
+
+    //显示当前网络不可用
+    void showNoNet();
+
+    //重试
+    void onRetry();
+}
