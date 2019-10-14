bff66153d51cc6a1fdce7d4af0f74794ee33729d
王静波 <wangjingbo3@qq.com>
2019-10-14 15:45:38 +0800

    Update BasePresenter.java

--- app/src/main/java/com/zynet/bobo/base/BasePresenter.java
+++ app/src/main/java/com/zynet/bobo/base/BasePresenter.java
@@ -1,5 +1,7 @@
 package com.zynet.bobo.base;
 
+import com.trello.rxlifecycle2.components.support.RxAppCompatActivity;
+import com.zynet.bobo.mvp.IBaseView;
 import com.zynet.bobo.mvp.http.RequestClient;
 
 /**
@@ -7,25 +9,27 @@ import com.zynet.bobo.mvp.http.RequestClient;
  * @date 2019/10/7 0007
  * describe
  */
-public class BasePresenter<T extends BaseContract.BaseView> implements BaseContract.BasePresenter<T> {
+public class BasePresenter<T extends IBaseView> {
     /**
      * 网络请求
      */
     protected RequestClient mRequestClient;
 
-    public BasePresenter() {
-        mRequestClient = RequestClient.getInstance();
-    }
+    protected RxAppCompatActivity mContext;
 
     protected T mView;
 
-    @Override
-    public void attachView(T view) {
+    public BasePresenter(RxAppCompatActivity rxAppCompatActivity, IBaseView iBaseView) {
+        mRequestClient = RequestClient.getInstance();
+        this.mContext = rxAppCompatActivity;
+        this.mView = (T) iBaseView;
+    }
+
+    protected void attachView(T view) {
         this.mView = view;
     }
 
-    @Override
-    public void detachView() {
+    protected void detachView() {
         if (mView != null) {
             mView = null;
         }
