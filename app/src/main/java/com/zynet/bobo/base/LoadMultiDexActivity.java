4815b27d08fc83308de5e7888c35e8d405ad190a
王静波 <wangjingbo3@qq.com>
2019-10-21 09:45:35 +0800

    Create LoadMultiDexActivity.java

---
+++ app/src/main/java/com/zynet/bobo/base/LoadMultiDexActivity.java
@@ -0,0 +1,67 @@
+public class LoadMultiDexActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoadMultiDexActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_load_multi_dex);
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+
+                loadMultiDex();
+            }
+        };
+        thread.setName("multi_dex");
+        thread.start();
+
+        showLoadingDialog();
+    }
+
+
+    private void loadMultiDex() {
+        Log.d(TAG, "MultiDex.install 开始: ");
+        long startTime = System.currentTimeMillis();
+        MultiDex.install(LoadMultiDexActivity.this);
+        Log.d(TAG, "MultiDex.install 结束，耗时: " + (System.currentTimeMillis() - startTime));
+
+        try {
+            //模拟MultiDex耗时很久的情况
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        aftetMultiDex();
+    }
+
+    private void aftetMultiDex() {
+        deleteTempFile(this);
+
+        //将这个进程杀死
+        Log.d(TAG, "aftetMultiDex: ");
+        finish();
+        Process.killProcess(Process.myPid());
+    }
+
+    private void deleteTempFile(Context context) {
+        try {
+            File file = new File(context.getCacheDir().getAbsolutePath(), "load_dex.tmp");
+            if (file.exists()) {
+                boolean delete = file.delete();
+                Log.d(TAG, "deleteTempFile: ");
+            }
+        } catch (Throwable th) {
+            th.printStackTrace();
+        }
+    }
+
+    private void showLoadingDialog() {
+        new AlertDialog.Builder(this)
+                .setMessage("加载中，请稍后...")
+                .show();
+    }
+
+}
