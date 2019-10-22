aa5f1599e0de671625caa40dc86dcc47b4c51b66
王静波 <wangjingbo3@qq.com>
2019-10-22 12:41:09 +0800

    Update MainActivity.java

--- app/src/main/java/com/zynet/bobo/MainActivity.java
+++ app/src/main/java/com/zynet/bobo/MainActivity.java
@@ -1,17 +1,24 @@
 package com.zynet.bobo;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.viewpager.widget.ViewPager;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
+import com.chaychan.library.BottomBarItem;
+import com.chaychan.library.BottomBarLayout;
 import com.zynet.bobo.base.SmartFragmentStatePagerAdapter;
 import com.zynet.bobo.ui.fragment.AboutFragment;
 import com.zynet.bobo.ui.fragment.HomeFragment;
@@ -35,15 +42,11 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.tvTitle)
     TitleView tvTitle;
     @BindView(R.id.viewpager)
-    ViewPager2 viewPager;
-    @BindView(R.id.tv1)
-    TextView tv1;
-    @BindView(R.id.tv2)
-    TextView tv2;
-    @BindView(R.id.tv3)
-    TextView tv3;
+    ViewPager viewPager;
 
     private List<Fragment> mFragments = new ArrayList<>();
+
+    private RotateAnimation mRotateAnimation;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -53,65 +56,86 @@ public class MainActivity extends AppCompatActivity {
         initView();
     }
 
-
     public void initView() {
+        BottomBarLayout mBottomBarLayout = findViewById(R.id.bbl);
         tvTitle.setTitle("首页");
         tvTitle.setBackImageGone(true);
         mFragments.add(new HomeFragment());
         mFragments.add(new AboutFragment());
         mFragments.add(new MyFragment());
-        //viewPager.setUserInputEnabled(false);
         viewPager.setCurrentItem(0);
-        viewPager.setAdapter(new FragmentStateAdapter(this) {
-            @NonNull
-            @Override
-            public Fragment createFragment(int position) {
-                return mFragments.get(position);
+        viewPager.setAdapter(new MyFragmentStateAdapter(getSupportFragmentManager()));
+        mBottomBarLayout.setViewPager(viewPager);
+        mBottomBarLayout.setSmoothScroll(true);
+
+        mBottomBarLayout.setOnItemSelectedListener((bottomBarItem, previousPosition, currentPosition) -> {
+            Log.i("MainActivity", "position: " + currentPosition);
+            if (currentPosition == 0) {
+                //如果是第一个，即首页
+                if (previousPosition == currentPosition) {
+                    //如果是在原来位置上点击,更换首页图标并播放旋转动画
+                    if (mRotateAnimation != null && !mRotateAnimation.hasEnded()) {
+                        //如果当前动画正在执行
+                        return;
+                    }
+
+                    //     bottomBarItem.setSelectedIcon(R.mipmap.tab_loading);//更换成加载图标
+
+                    //播放旋转动画
+                    if (mRotateAnimation == null) {
+                        mRotateAnimation = new RotateAnimation(0, 360,
+                                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+                                0.5f);
+                        mRotateAnimation.setDuration(800);
+                        mRotateAnimation.setRepeatCount(-1);
+                    }
+                    ImageView bottomImageView = bottomBarItem.getImageView();
+                    bottomImageView.setAnimation(mRotateAnimation);
+                    bottomImageView.startAnimation(mRotateAnimation);//播放旋转动画
+
+                    //模拟数据刷新完毕
+//                        mHandler.postDelayed(new Runnable() {
+//                            @Override
+//                            public void run() {
+//                                boolean tabNotChanged = mBottomBarLayout.getCurrentItem() == currentPosition; //是否还停留在当前页签
+//                                bottomBarItem.setSelectedIcon(R.mipmap.tab_home_selected);//更换成首页原来选中图标
+//                                cancelTabLoading(bottomBarItem);
+//                            }
+//                        }, 3000);
+                    return;
+                }
             }
 
-            @Override
-            public int getItemCount() {
-                return 3;
-            }
+            //如果点击了其他条目
+            BottomBarItem bottomItem = mBottomBarLayout.getBottomItem(0);
+            bottomItem.setSelectedIcon(R.mipmap.tab_home_selected);//更换为原来的图标
+
+            cancelTabLoading(bottomItem);//停止旋转动画
         });
 
-        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                super.onPageSelected(position);
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-                super.onPageScrollStateChanged(state);
-            }
-        });
+        mBottomBarLayout.setUnread(0, 20);//设置第一个页签的未读数为20
+        mBottomBarLayout.setUnread(1, 1001);//设置第二个页签的未读数
+        mBottomBarLayout.showNotify(2);//设置第三个页签显示提示的小红点
+        mBottomBarLayout.setMsg(3, "NEW");//设置第四个页签显示NEW提示文字
     }
 
 
-    @OnClick({R.id.tv1, R.id.tv2, R.id.tv3})
-    public void onViewClicked(View view) {
-        switch (view.getId()) {
-            case R.id.tv1:
-                viewPager.setCurrentItem(0);
-                break;
-            case R.id.tv2:
-                viewPager.setCurrentItem(1);
-                break;
-            case R.id.tv3:
-                viewPager.setCurrentItem(2);
-                break;
-            default:
-                break;
+    /**
+     * 停止首页页签的旋转动画
+     */
+    private void cancelTabLoading(BottomBarItem bottomItem) {
+        Animation animation = bottomItem.getImageView().getAnimation();
+        if (animation != null) {
+            animation.cancel();
         }
     }
 
-    class MyFragmentStateAdapter extends SmartFragmentStatePagerAdapter{
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    class MyFragmentStateAdapter extends SmartFragmentStatePagerAdapter {
 
         public MyFragmentStateAdapter(FragmentManager fragmentManager) {
             super(fragmentManager);
@@ -120,12 +144,12 @@ public class MainActivity extends AppCompatActivity {
         @NonNull
         @Override
         public Fragment getItem(int position) {
-            return null;
+            return mFragments.get(position);
         }
 
         @Override
         public int getCount() {
-            return 0;
+            return mFragments.size();
         }
     }
 
